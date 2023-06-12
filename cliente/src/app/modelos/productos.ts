ff917f8b20dce06e3789cab720a7872ac9053329
marcelolav <marcelo.lavandeira@gmail.com>
2023-06-11 23:58:04 -0300

    ok7

---
+++ cliente/src/app/modelos/productos.ts
@@ -0,0 +1,11 @@
+export interface Producto {
+  idproducto?: number;
+  codigobarra?: string;
+  nombre?: string;
+  descripcion?: string;
+  precio?: number;
+  preciocompra: number;
+  existencia?: number;
+  preciorefdolar?: number;
+  rubroid?: number;
+}
