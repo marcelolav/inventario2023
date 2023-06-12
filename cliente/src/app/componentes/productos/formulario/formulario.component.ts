7f5891697b9535205fb551efa66c03215957c8ab
marcelolav <marcelo.lavandeira@gmail.com>
2023-06-11 23:57:47 -0300

    ok

--- cliente/src/app/componentes/productos/formulario/formulario.component.ts
+++ cliente/src/app/componentes/productos/formulario/formulario.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/modelos/productos';
 
 @Component({
   selector: 'app-formulario-productos',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css'],
 })
-export class FormularioProductosComponent {}
+
+export class FormularioProductosComponent implements OnInit {
+
+
+  producto: Producto = {
+    idproducto: 0,
+    codigobarra: '',
+    nombre: '',
+    descripcion: '',
+    precio: 0,
+    preciocompra: 0,
+    existencia: 0,
+    preciorefdolar:0,
+    rubroid: 0,
+  };
+  
+
+  constructor() { }
+
+  ngOnInit() {
+}
