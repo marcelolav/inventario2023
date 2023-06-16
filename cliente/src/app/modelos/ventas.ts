8b60ba78e3f34e7efded0d4eb49f40da120634a6
Marcelo Lavandeira <51520483+marcelolav@users.noreply.github.com>
2023-06-16 10:27:24 -0300

    Actualizar ventas.ts
    se debe generar una tabla clientes y en ventas model cambiar

--- cliente/src/app/modelos/ventas.ts
+++ cliente/src/app/modelos/ventas.ts
@@ -1,10 +1,11 @@
 export interface Venta {
   idventas?: number;
   comprobante?: number;
-  cliente?: string;
+  cliente?: string;  // registro completo
   fecha?: Date;
   producto?: string;
   cantidad?: number;
   preciounitario?: number;
   preciosubtotal?: number;
 }
+// definir registro de cliente y funcion get
